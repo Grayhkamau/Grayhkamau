68729714654c2bfc093dab668349fd419bf8ea42
Grayhkamau <58790731+Grayhkamau@users.noreply.github.com>
2021-01-28 21:43:13 +0300

    Add files via upload

---
+++ bus_fare_challenge.py
@@ -0,0 +1,55 @@
+# WRITE YOUR CODE SOLUTION HERE
+from datetime import datetime
+
+date = datetime.now()
+
+day = print('day': + str(date.strftime('%a')))
+
+if today_date = datetime.datetime(2020, 1, 28):
+    fare = int(100)
+    thursday = print('Day': + str(today_date.strftime('%a')))
+    print('Date: ' + str(today_date))
+    print(thursday)
+    print('fare: '+ str(fare)
+
+elif today_date = datetime.datetime(2020, 1, 29):
+    fare = int(100)
+    friday = print('Day': + str(today_date.strftime('%a')))
+    print('your fare is' + str(fare))
+    print(friday)
+    print('fare: '+ str(fare)
+
+elif today_date = datetime.datetime(2020, 1, 30):
+    fare = int(60)
+    saturday = print('Day': + str(today_date.strftime('%a')))
+    print('your fare is' + str(fare))
+    print(saturday)
+    print('fare: '+ str(fare)
+    
+elif today_date = datetime.datetime(2020, 1, 31):
+    fare = int(80)
+    sunday = print('Day': + str(today_date.strftime('%a')))
+    print('your fare is' + str(fare))
+    print(sunday)
+    print('fare: '+ str(fare)
+
+elif today_date = datetime.datetime(2020, 2, 1):
+    fare = int(100)
+    monday = print('Day': + str(today_date.strftime('%a')))
+    print('your fare is' + str(fare))
+    print(monday)
+    print('fare: '+ str(fare)
+
+elif today_date = datetime.datetime(2020, 2, 2):
+    fare = int(100)
+    tuesday = print('Day': + str(today_date.strftime('%a')))
+    print('your fare is' + str(fare))
+    print(tuesday)
+    print('fare: '+ str(fare)
+
+else today_date = datetime.datetime(2020, 2, 3):
+    fare = int(100)
+    wednesday = print('Day': + str(today_date.strftime('%a')))
+    print('your fare is' + str(fare))
+    print(wednesday)
+    print('fare: '+ str(fare)
