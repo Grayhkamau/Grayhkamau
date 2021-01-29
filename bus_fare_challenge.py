8437bd679bb1c5f12e49a29447c2bcb8362bdd91
Grayhkamau <58790731+Grayhkamau@users.noreply.github.com>
2021-01-29 09:21:08 +0300

    Update bus_fare_challenge.py

--- bus_fare_challenge.py
+++ bus_fare_challenge.py
@@ -3,53 +3,65 @@ from datetime import datetime
 
 date = datetime.now()
 
+fare = int(100)
+fare_2 = int(80)
+fare_3 = int(60)
+
 day = print('day': + str(date.strftime('%a')))
+day = print('day: ' + str(date.strftime('%a')))
+thursday = print('Day: ' + str(today_date.strftime('%a')))
+friday = print('Day: ' + str(today_date.strftime('%a')))
+saturday = print('Day: ' + str(today_date.strftime('%a')))
+sunday = print('Day: ' + str(today_date.strftime('%a')))
+monday = print('Day: ' + str(today_date.strftime('%a')))
+tuesday = print('Day: ' + str(today_date.strftime('%a')))
+wednesday = print('Day: ' + str(today_date.strftime('%a')))
 
 if today_date = datetime.datetime(2020, 1, 28):
-    fare = int(100)
-    thursday = print('Day': + str(today_date.strftime('%a')))
+    #fare = int(100)
+    #thursday = print('Day: ' + str(today_date.strftime('%a')))
     print('Date: ' + str(today_date))
     print(thursday)
     print('fare: '+ str(fare)
 
 elif today_date = datetime.datetime(2020, 1, 29):
-    fare = int(100)
-    friday = print('Day': + str(today_date.strftime('%a')))
-    print('your fare is' + str(fare))
+    #fare = int(100)
+    #friday = print('Day: ' + str(today_date.strftime('%a')))
+    print('Date: ' + str(today_date))
     print(friday)
     print('fare: '+ str(fare)
 
 elif today_date = datetime.datetime(2020, 1, 30):
-    fare = int(60)
-    saturday = print('Day': + str(today_date.strftime('%a')))
-    print('your fare is' + str(fare))
+    #fare = int(60)
+    #saturday = print('Day: ' + str(today_date.strftime('%a')))
+    print('Date: ' + str(today_date))
     print(saturday)
-    print('fare: '+ str(fare)
+    print('fare: '+ str(fare_3)
     
 elif today_date = datetime.datetime(2020, 1, 31):
-    fare = int(80)
-    sunday = print('Day': + str(today_date.strftime('%a')))
-    print('your fare is' + str(fare))
+    #fare = int(80)
+    #sunday = print('Day: ' + str(today_date.strftime('%a')))
+    print('Date: ' + str(today_date))
     print(sunday)
-    print('fare: '+ str(fare)
+    print('fare: '+ str(fare_2)
 
 elif today_date = datetime.datetime(2020, 2, 1):
-    fare = int(100)
-    monday = print('Day': + str(today_date.strftime('%a')))
-    print('your fare is' + str(fare))
+    #fare = int(100)
+    #monday = print('Day: ' + str(today_date.strftime('%a')))
+    print('Date:' + str(today_date))
     print(monday)
     print('fare: '+ str(fare)
 
 elif today_date = datetime.datetime(2020, 2, 2):
-    fare = int(100)
-    tuesday = print('Day': + str(today_date.strftime('%a')))
-    print('your fare is' + str(fare))
+    #fare = int(100)
+    #tuesday = print('Day: ' + str(today_date.strftime('%a')))
+    print('Date :' + str(today_date))
     print(tuesday)
     print('fare: '+ str(fare)
 
 else today_date = datetime.datetime(2020, 2, 3):
-    fare = int(100)
-    wednesday = print('Day': + str(today_date.strftime('%a')))
-    print('your fare is' + str(fare))
+    #fare = int(100)
+    #wednesday = print('Day': + str(today_date.strftime('%a')))
+    print('Date: ' + str(today_date))
     print(wednesday)
     print('fare: '+ str(fare)
